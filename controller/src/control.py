b80212798b5c3b53ec2d22933a569e0e35faea39
Zac Orndorff <zorndorff@gmail.com>
2019-09-29 18:05:00 +0100

    update

--- controller/src/control.py
+++ controller/src/control.py
@@ -1,10 +1,16 @@
-
+#!/usr/bin/python3
+import time
 from adafruit_motorkit import MotorKit
 kit = MotorKit()
 
 
-kit.motor1.throttle = 0.5
 
-sleep(2000)
+kit.motor1.throttle = 0.5
+kit.motor2.throttle = 0.5
+
+time.sleep(10)
 
 kit.motor1.throttle = None
+kit.motor2.throttle = None
+
+exit(0)
