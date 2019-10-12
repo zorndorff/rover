39b2b84da0401b3beddfc3fddadc98b859f36b64
Zac O <zorndorff@gmail.com>
2019-10-12 09:20:38 -0500

    update

--- controller/src/control.py
+++ controller/src/control.py
@@ -1,41 +1,21 @@
 #!/usr/bin/python3
 import time
-import pygame
-from pygame.locals import *
 from adafruit_motorkit import MotorKit
-kit = MotorKit()
+# kit = MotorKit()
 
-forward_throttle = 0
-steering_throttle = 0 
+# forward_throttle = 0
+# steering_throttle = 0 
 
-kit.motor2.throttle = forward_throttle
-kit.motor1.throttle = forward_throttle
-kit.motor3.throttle = steering_throttle 
+# kit.motor2.throttle = forward_throttle
+# kit.motor1.throttle = forward_throttle
+# kit.motor3.throttle = steering_throttle 
 
-pygame.init()
+# num = 0
+# done = False
+# while not done:        
+#     kit.motor2.throttle = forward_throttle 
+#     kit.motor1.throttle = forward_throttle
+#     kit.motor3.throttle = steering_throttle
 
-num = 0
-done = False
-while not done:
-
-    pygame.event.pump()
-    keys = pygame.key.get_pressed()
-    if keys[K_ESCAPE]:
-        done = True
-    if keys[K_SPACE]:
-        forward_throttle = 0
-        steering_throttle = 0
-    if keys[K_LEFT]:
-        steering_throttle += 0.1 
-    if keys[K_RIGHT]:
-        steering_throttle += -0.1 
-    if keys[K_UP]:
-        forward_throttle += -0.1
-    if keys[K_DOWN]:
-        forward_throttle += 0.1
-        
-    kit.motor2.throttle = forward_throttle 
-    kit.motor1.throttle = forward_throttle
-    kit.motor3.throttle = steering_throttle
-
-    print(forward_throttle + "::" + steering_throttle)
+#     print(forward_throttle + "::" + steering_throttle)
+#     time.sleep(5)
