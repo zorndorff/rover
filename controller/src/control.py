a4b3e83c5fe0222051b8ae540376252e7f35a900
Zac Orndorff <zorndorff@gmail.com>
2019-10-12 14:38:44 +0100

    control scripts

--- controller/src/control.py
+++ controller/src/control.py
@@ -1,12 +1,41 @@
 #!/usr/bin/python3
 import time
+import pygame
+from pygame.locals import *
 from adafruit_motorkit import MotorKit
 kit = MotorKit()
 
+forward_throttle = 0
+steering_throttle = 0 
 
+kit.motor2.throttle = forward_throttle
+kit.motor1.throttle = forward_throttle
+kit.motor3.throttle = steering_throttle 
 
-kit.motor2.throttle = -1
-kit.motor1.throttle = -1
+pygame.init()
 
+num = 0
+done = False
+while not done:
 
-exit(0)
+    pygame.event.pump()
+    keys = pygame.key.get_pressed()
+    if keys[K_ESCAPE]:
+        done = True
+    if keys[K_SPACE]:
+        forward_throttle = 0
+        steering_throttle = 0
+    if keys[K_LEFT]:
+        steering_throttle += 0.1 
+    if keys[K_RIGHT]:
+        steering_throttle += -0.1 
+    if keys[K_UP]:
+        forward_throttle += -0.1
+    if keys[K_DOWN]:
+        forward_throttle += 0.1
+        
+    kit.motor2.throttle = forward_throttle 
+    kit.motor1.throttle = forward_throttle
+    kit.motor3.throttle = steering_throttle
+
+    print(forward_throttle + "::" + steering_throttle)
--- controller/src/forward_right.py
+++ controller/src/forward_right.py
@@ -7,10 +7,14 @@ kit = MotorKit()
 
 kit.motor2.throttle = -0.5
 kit.motor1.throttle = -0.5
+kit.motor3.throttle = 0.5
 
 time.sleep(0.5)
 
 kit.motor2.throttle = None
 kit.motor1.throttle = None
+kit.motor3.throttle = None
+
+
 
 exit(0)
